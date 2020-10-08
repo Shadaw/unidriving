43a3cbebf91038363de833c69dfac8c0458df21b
Gustavo Silva <gustavoalprofissional@gmail.com>
2020-10-08 18:15:41 -0300

    :heavy_plus_sign: adding cors

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import cors from 'cors';
 import routes from './routes';
 import './database';
 
@@ -8,6 +9,7 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 app.listen(process.env.PORT || 3333, () => {
